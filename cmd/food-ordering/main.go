60c00fef1a5158914af53df837fe8e5ab88623bb
unknown <mehdi.mortazavian@gmail.com>
2024-04-02 12:44:23 +0330

    swagger added to the project and try to use is login middleware

--- cmd/food-ordering/main.go
+++ cmd/food-ordering/main.go
@@ -4,7 +4,23 @@ import (
 	"Food-Ordering/internal/repository"
 	"Food-Ordering/internal/routes"
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 
 func main() {
 
@@ -19,8 +35,11 @@ func main() {
 
 	routes.UserRouts(e)
 
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
 	err = e.Start(":8080")
 	if err != nil {
 		return
 	}
+
 }
