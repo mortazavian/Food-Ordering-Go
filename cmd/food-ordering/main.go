a8fa0b188ad6d7ad2c637a8bb0ba29c09db6cde4
unknown <mehdi.mortazavian@gmail.com>
2024-04-03 20:31:46 +0330

    jwt fixed

--- cmd/food-ordering/main.go
+++ cmd/food-ordering/main.go
@@ -4,42 +4,33 @@ import (
 	"Food-Ordering/internal/repository"
 	"Food-Ordering/internal/routes"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
-// @title Swagger Example API
-// @version 1.0
-// @description This is a sample server Petstore server.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host petstore.swagger.io
-// @BasePath /v2
-
 func main() {
-
 	db := repository.NewGormPostgres()
 
 	err := repository.Migration(db)
 	if err != nil {
+		// Handle migration error
 		return
 	}
 
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	routes.UserRouts(e)
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
+	// Start server
 	err = e.Start(":8080")
 	if err != nil {
+		// Handle server start error
 		return
 	}
-
 }
